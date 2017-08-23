49660be6c5f5540b544ab36f50e2ce976175f5fe
Kom Sihon <rsihon@gmail.com>
2017-08-23 05:34:04 +0100

    STATUS_CHOICES and GENDER constants added

--- core/constants.py
+++ core/constants.py
@@ -1,2 +1,15 @@
+from django.utils.translation import gettext_lazy as _
 
 PENDING = 'Pending'
+CONFIRMED = 'Confirmed'
+STATUS_CHOICES = (
+    (PENDING, _('Pending')),
+    (CONFIRMED, _('Confirmed'))
+)
+
+MALE = 'Male'
+FEMALE = 'Female'
+GENDER_CHOICES = (
+    (MALE, _('Male')),
+    (FEMALE, _('Female'))
+)
