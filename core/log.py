090f891828e321924f78174357983cfd7df535da
Kom Sihon <rsihon@gmail.com>
2017-08-25 12:36:39 +0100

    ikwen Apps default logger

---
+++ core/log.py
@@ -0,0 +1,53 @@
+import logging
+try:
+    from logging.config import dictConfig
+except ImportError:
+    from django.utils.dictconfig import dictConfig
+
+getLogger = logging.getLogger
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'default': {
+            'format': '%(asctime)-27s %(message)s'
+        }
+    },
+    'filters': {
+        'require_lower_than_error': {
+            '()': 'ikwen.core.log.RequireLowerThanError'
+        }
+    },
+    'handlers': {
+        'info_log_handler': {
+            'level': 'DEBUG',
+            'filters': ['require_lower_than_error'],
+            'formatter': 'default',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': '/var/www/ikwen_info.log',
+            'maxBytes': 10000,
+            'backupCount': 4
+        },
+        'error_log_handler': {
+            'level': 'ERROR',
+            'formatter': 'default',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': '/var/www/ikwen_error.log',
+            'maxBytes': 10000,
+            'backupCount': 4
+        }
+    },
+    'loggers': {
+        'ikwen': {
+            'handlers': ['info_log_handler', 'error_log_handler'],
+            'level': 'DEBUG',
+            'propagate': True,
+        }
+    }
+}
+
+
+class RequireLowerThanError(logging.Filter):
+    def filter(self, record):
+        return record.levelno < 40
