5fefd051b143d0189fe4599a70f1d546344570e4
Kom Sihon <rsihon@gmail.com>
2018-03-17 10:28:09 +0100

    Logging added to deploy script run. Module Blog is created because retailers platforms are now based on WebNode

--- partnership/cloud_setup.py
+++ partnership/cloud_setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import shutil
 import os
-
+import logging
 import subprocess
 from threading import Thread
 
@@ -13,6 +13,9 @@ from django import forms
 from django.template import Context
 from django.template.defaultfilters import slugify
 from django.template.loader import get_template
+from ikwen.theming.models import Template, Theme
+
+from ikwen.billing.mtnmomo.views import MTN_MOMO
 from permission_backend_nonrel.models import UserPermissionList, GroupPermissionList
 
 from ikwen.accesscontrol.models import SUDO, Member
@@ -26,19 +29,18 @@ from ikwen.partnership.models import PartnerProfile
 from ikwen.billing.utils import get_next_invoice_number
 from django.utils.translation import gettext as _
 
-from ikwen.billing.models import Invoice, InvoicingConfig
+from ikwen.billing.models import Invoice, InvoicingConfig, PaymentMean
 
 from ikwen.core.utils import add_database_to_settings, add_event, get_mail_content, get_service_instance
 
 from ikwen.accesscontrol.backends import UMBRELLA
 
-from ikwen.core.models import Service, SERVICE_DEPLOYED, OperatorWallet
+from ikwen.core.models import Service, SERVICE_DEPLOYED, OperatorWallet, Module
 
-from ikwen.core.tools import generate_django_secret_key, generate_random_key
+from ikwen.core.tools import generate_django_secret_key, generate_random_key, reload_server
 from datetime import datetime, timedelta
 
-
-__author__ = 'Kom Sihon'
+logger = logging.getLogger('ikwen')
 
 
 if getattr(settings, 'LOCAL_DEV', False):
@@ -62,7 +64,7 @@ class DeploymentForm(forms.Form):
     monthly_cost = forms.FloatField()
 
 
-def deploy(app, member, project_name, billing_plan, monthly_cost,
+def deploy(app, member, project_name, billing_plan, monthly_cost, theme,
            billing_cycle, invoice_entries, is_pro_version=False):
     project_name_slug = slugify(project_name)  # Eg: slugify('Cool Shop') = 'cool-shop'
     ikwen_name = project_name_slug.replace('-', '')  # Eg: cool-shop --> 'coolshop'
@@ -96,37 +98,49 @@ def deploy(app, member, project_name, billing_plan, monthly_cost,
     website_home_folder = CLOUD_FOLDER + ikwen_name
     media_root = MEDIA_ROOT + ikwen_name + '/'
     media_url = MEDIA_URL + ikwen_name + '/'
-    if not os.path.exists(media_root):
+    default_images_folder = CLOUD_FOLDER + '000Tpl/images/000Default'
+    if os.path.exists(default_images_folder):
+        if os.path.exists(media_root):
+            shutil.rmtree(media_root)
+        shutil.copytree(default_images_folder, media_root)
+        logger.debug("Media folder '%s' successfully created from '%s'" % (media_root, default_images_folder))
+    elif not os.path.exists(media_root):
         os.makedirs(media_root)
-    if os.path.exists(website_home_folder):
-        shutil.rmtree(website_home_folder)
+        logger.debug("Media folder '%s' successfully created empty" % media_root)
     favicons_folder = media_root + 'favicons'
     if not os.path.exists(favicons_folder):
         os.makedirs(favicons_folder)
     if os.path.exists(website_home_folder):
         shutil.rmtree(website_home_folder)
     shutil.copytree(app_folder, website_home_folder)
+    logger.debug("Service folder '%s' successfully created" % website_home_folder)
+
+    settings_template = 'partnership/cloud_setup/settings.html'
 
     service = Service(member=member, app=app, project_name=project_name, project_name_slug=ikwen_name, domain=domain,
                       database=database, url='http://' + domain, domain_type=domain_type, expiry=expiry,
                       admin_url='http://' + admin_url, api_signature=api_signature,
-                      billing_cycle=billing_cycle, monthly_cost=monthly_cost, version=Service.TRIAL)
+                      billing_cycle=billing_cycle, monthly_cost=monthly_cost, version=Service.TRIAL,
+                      home_folder=website_home_folder, settings_template=settings_template)
     service.save(using=UMBRELLA)
 
     # Re-create settings.py file as well as apache.conf file for the newly created project
     secret_key = generate_django_secret_key()
     allowed_hosts = '"%s", "www.%s"' % (domain, domain)
-    settings_tpl = get_template('partnership/cloud_setup/settings.html')
+    settings_tpl = get_template(settings_template)
     settings_context = Context({'secret_key': secret_key, 'ikwen_name': ikwen_name,
                                 'service': service, 'static_root': STATIC_ROOT, 'static_url': STATIC_URL,
                                 'media_root': media_root, 'media_url': media_url,
                                 'allowed_hosts': allowed_hosts, 'debug': getattr(settings, 'DEBUG', False)})
-    fh = open(website_home_folder + '/conf/settings.py', 'w')
+    settings_file = website_home_folder + '/conf/settings.py'
+    fh = open(settings_file, 'w')
     fh.write(settings_tpl.render(settings_context))
     fh.close()
+    logger.debug("Settings file '%s' successfully created" % settings_file)
 
     # Import template database and set it up
-    subprocess.call(['mongorestore', '-d', database, CLOUD_FOLDER + '000Tpl/DB/000Default'])
+    host = getattr(settings, 'DATABASES')['default'].get('HOST', '127.0.0.1')
+    subprocess.call(['mongorestore', '--host', host, '-d', database, CLOUD_FOLDER + '000Tpl/DB/000Default'])
     add_database_to_settings(database)
     for group in Group.objects.using(database).all():
         try:
@@ -163,9 +177,40 @@ def deploy(app, member, project_name, billing_plan, monthly_cost,
 
     app.save(using=database)
     member.save(using=database)
+    logger.debug("Member %s access rights successfully set for service %s" % (member.username, pname))
 
-    FlatPage.objects.using(database).create(url=FlatPage.AGREEMENT, title=FlatPage.AGREEMENT)
-    FlatPage.objects.using(database).create(url=FlatPage.LEGAL_MENTIONS, title=FlatPage.LEGAL_MENTIONS)
+    # Copy payment means to local database
+    for mean in PaymentMean.objects.using(UMBRELLA).all():
+        if mean.slug == 'paypal':
+            mean.action_url_name = 'shopping:paypal_set_checkout'
+        if mean.slug == MTN_MOMO:
+            mean.is_main = True
+            mean.is_active = True
+        else:
+            mean.is_main = False
+            if is_pro_version:
+                mean.is_active = True
+            else:
+                mean.is_active = False
+        mean.save(using=database)
+        logger.debug("PaymentMean %s created in database: %s" % (mean.slug, database))
+
+    # Copy themes to local database
+    for template in Template.objects.using(UMBRELLA).all():
+        template.save(using=database)
+    for th in Theme.objects.using(UMBRELLA).all():
+        th.save(using=database)
+    logger.debug("Template and theme successfully bound for service: %s" % pname)
+
+    # Create Module Blog
+    module_blog = Module.objects.using(UMBRELLA).get(slug='module_blog')
+    module_blog.is_active = True
+    module_blog.save(using=database)
+    logger.debug("Module Blog created for service: %s" % pname)
+
+    FlatPage.objects.using(database).create(url=FlatPage.AGREEMENT, title=FlatPage.AGREEMENT.capitalize())
+    FlatPage.objects.using(database).create(url=FlatPage.LEGAL_MENTIONS,
+                                            title=FlatPage.LEGAL_MENTIONS.capitalize().replace('-', ' '))
 
     # Add member to SUDO Group
     obj_list, created = UserPermissionList.objects.using(database).get_or_create(user=member)
@@ -175,13 +220,14 @@ def deploy(app, member, project_name, billing_plan, monthly_cost,
     OperatorWallet.objects.using('wallets').create(nonrel_id=service.id)
     mail_signature = "%s<br>" \
                      "<a href='%s'>%s</a>" % (project_name, 'http://' + domain, domain)
-    config = PartnerProfile(service=service, is_pro_version=is_pro_version,
+    config = PartnerProfile(service=service, is_pro_version=is_pro_version, theme=theme,
                             currency_code='XAF', currency_symbol='XAF', signature=mail_signature,
                             company_name=project_name, contact_email=member.email, contact_phone=member.phone)
     config.save(using=UMBRELLA)
     base_config = config.get_base_config()
     base_config.save(using=UMBRELLA)
     service.save(using=database)
+    theme.save(using=database)  # Causes theme to be routed to the newly created database
     config.save(using=database)
 
     InvoicingConfig.objects.using(database).create()
@@ -196,8 +242,9 @@ def deploy(app, member, project_name, billing_plan, monthly_cost,
     fh.write(apache_tpl.render(apache_context))
     fh.close()
 
-    subprocess.call(['sudo', 'ln', '-sf', website_home_folder + '/apache.conf', '/etc/apache2/sites-enabled/' + domain + '.conf'])
-    subprocess.call(['sudo', 'service', 'apache2', 'reload'])
+    vhost = '/etc/apache2/sites-enabled/' + domain + '.conf'
+    subprocess.call(['sudo', 'ln', '-sf', website_home_folder + '/apache.conf', vhost])
+    logger.debug("Apache Virtual Host '%s' successfully created" % vhost)
 
     # Send notification and Invoice to customer
     number = get_next_invoice_number()
@@ -221,6 +268,10 @@ def deploy(app, member, project_name, billing_plan, monthly_cost,
                                     extra_context={'service_activated': service, 'invoice': invoice,
                                                    'member': member, 'invoice_url': invoice_url})
     msg = EmailMessage(subject, html_content, sender, [member.email])
+    msg.bcc = ['contact@ikwen.com']
     msg.content_subtype = "html"
     Thread(target=lambda m: m.send(), args=(msg, )).start()
+    logger.debug("Notice email submitted to %s" % member.email)
+    Thread(target=reload_server).start()
+    logger.debug("Apache Scheduled to reload in 5s")
     return service
