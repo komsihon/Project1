70178f3beca03bc49c84a60df49ec162399e63c6
Kom Sihon <rsihon@gmail.com>
2019-07-25 16:35:40 +0100

    Bug fix: index() filter now catches IndexError

--- core/templatetags/index.py
+++ core/templatetags/index.py
@@ -9,4 +9,7 @@ def index(a_list, i):
     Given a list. Returns the item at index i
     Allows dynamin list indexing in templates
     """
-    return a_list[int(i)]
+    try:
+        return a_list[int(i)]
+    except IndexError:
+        return None
