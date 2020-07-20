318ab68682fea092cf11a33b7b2894717c68901c
Kom Sihon <rsihon@gmail.com>
2020-07-20 22:49:13 +0100

    Small fixes

--- partnership/urls.py
+++ partnership/urls.py
@@ -5,19 +5,20 @@ from ikwen.core.views import load_event_content
 
 from ikwen.flatpages.views import FlatPageView
 
-from ikwen.partnership.views import Dashboard, ApplicationList, ServiceList, ChangeService, DeployCloud
+from ikwen.partnership.views import Dashboard, ApplicationList, ServiceList, ChangeService, DeployCloud, AdminHome
 from ikwen_webnode.webnode.views import Home
 
 urlpatterns = patterns(
     '',
     url(r'^$', Home.as_view(), name='home'),
     url(r'^kakocase/', include('ikwen_kakocase.kakocase.urls', namespace='kakocase')),
-    url(r'^shavida/', include('ikwen_shavida.shavida.urls', namespace='shavida')),
+    # url(r'^shavida/', include('ikwen_shavida.shavida.urls', namespace='shavida')),
     url(r'^blog/', include('ikwen_webnode.blog.urls', namespace='blog')),
     url(r'^web/', include('ikwen_webnode.web.urls', namespace='web')),
     url(r'^items/', include('ikwen_webnode.items.urls', namespace='items')),
 
     url(r'^billing/', include('ikwen.billing.urls', namespace='billing')),
+    url(r'^ikwen/home/$', permission_required('accesscontrol.sudo')(AdminHome.as_view()), name='home'),
     url(r'^ikwen/dashboard/$', permission_required('accesscontrol.sudo')(Dashboard.as_view()), name='dashboard'),
     url(r'^ikwen/apps/$', permission_required('accesscontrol.sudo')(ApplicationList.as_view()), name='app_list'),
     url(r'^ikwen/services/$', permission_required('accesscontrol.sudo')(ServiceList.as_view()), name='service_list'),
